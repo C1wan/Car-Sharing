50dcff9dab9f71c62d490dd18adaea3f9abb0435
Ciwan Gülpinar <ciwangulpinar@yahoo.de>
2023-12-07 22:46:58 +0100

    added models

--- car_sharing/cars/models.py
+++ car_sharing/cars/models.py
@@ -4,10 +4,21 @@ from django.db import models
 # TODO : Felder für Models überlegen
 
 class Car(models.Model): 
-    make = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     year = models.PositiveSmallIntegerField(max_length=4)
+    consumption_type = models.PositiveSmallIntegerField(max_length=4)
+    latidude = models.DecimalField(max_digits=8)
+    longitude = models.DecimalField(max_digits=8)
     
+class Brand(models.Models):
+    brand = brand = models.CharField(max_length=255)
+    
+class Model(models.Model):
+    brand = models.CharField(max_length=255)
+      
+    def __str__(self):
+        return str(self.name)
     
     #hier db relation beahcten make sollte eine eigene tabelle
     #haben und model und jahr auch usw...
